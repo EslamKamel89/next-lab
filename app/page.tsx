22e3ae134b1ebadbade9b97fcea2eefdd245d49d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-23 19:00:00 +0200

    feat(home): add meals navigation links and adjust body positioning

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
+      <ul>
+        <li>
+          <Link href="/meals">Meal</Link>
+        </li>
+        <li>
+          <Link href="/meals/share">Share</Link>
+        </li>
+        <li>
+          <Link href="/meals/community">Community</Link>
+        </li>
+        <li>
+          <Link href="/meals/burger">burger</Link>
+        </li>
+      </ul>
     </div>
   );
 }
